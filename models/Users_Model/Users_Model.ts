8f637a82bdcd75535fb557d4133a5b31723ffc0f
AliDeWeb <alimoradi0business@gmail.com>
2024-06-12 04:59:14 -0700

    time stamps added to users schema

--- models/Users_Model/Users_Model.ts
+++ models/Users_Model/Users_Model.ts
@@ -3,45 +3,50 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 import userModelTypes from "./Users_Model.types";
 
-const usersSchema = new Schema<userModelTypes>({
-  role: {
-    type: String,
-    default: "user",
-    enum: ["user", "admin"],
-  },
-  name: {
-    type: String,
-    required: [true, "name is required"],
-  },
-  email: {
-    type: String,
-    unique: true,
-  },
-  phone: {
-    type: String,
-    unique: true,
-    required: [true, "phone number is required"],
-  },
-  password: {
-    type: String,
-    required: [true, "password is required"],
-    minLength: [8, "password should have 8 char at least"],
-    select: false,
-  },
-  confirmPassword: {
-    type: String,
-    required: [true, "Confirm Password is required"],
-    validate: {
-      validator: function (this: userModelTypes, el: string) {
-        return el === this.password;
+const usersSchema = new Schema<userModelTypes>(
+  {
+    role: {
+      type: String,
+      default: "user",
+      enum: ["user", "admin"],
+    },
+    name: {
+      type: String,
+      required: [true, "name is required"],
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    phone: {
+      type: String,
+      unique: true,
+      required: [true, "phone number is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+      minLength: [8, "password should have 8 char at least"],
+      select: false,
+    },
+    confirmPassword: {
+      type: String,
+      required: [true, "Confirm Password is required"],
+      validate: {
+        validator: function (this: userModelTypes, el: string) {
+          return el === this.password;
+        },
+        message: "رمز عبور ها یکسان نیستند!",
       },
-      message: "رمز عبور ها یکسان نیستند!",
+    },
+    profileImg: {
+      type: String,
     },
   },
-  profileImg: {
-    type: String,
+  {
+    timestamps: true,
   },
-});
+);
 
 usersSchema.pre(`save`, async function (next) {
   this.password = await bcrypt.hash(this.password, 12);
