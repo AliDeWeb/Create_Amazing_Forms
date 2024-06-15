945e3041d145e0bd8022708f4d7ab0c242b43950
AliDeWeb <alimoradi0business@gmail.com>
2024-06-15 05:55:19 -0700

    api features logic completed

--- controllers/User_Controllers/User_Controllers.ts
+++ controllers/User_Controllers/User_Controllers.ts
@@ -1,10 +1,16 @@
 import catchAsync from "../../utils/CatchAsync/CatchAsync";
 import { Response, Request, NextFunction } from "express";
 import usersModel from "../../models/Users_Model/Users_Model";
+import ApiFeatures from "../../utils/ApiFeatures/ApiFeatures";
 
 export const getAllUsers = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const users = await usersModel.find();
+    const query = new ApiFeatures(usersModel.find(), req.query)
+      .filter()
+      .sort()
+      .fields()
+      .paginate();
+    const users = await query.query;
 
     res.status(201).json({
       status: "success",
