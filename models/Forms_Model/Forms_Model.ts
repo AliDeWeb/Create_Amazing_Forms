c43e97ecbd687f6dd78079b11c23e5ae15d8d9d3
AliDeWeb <alimoradi0business@gmail.com>
2024-06-16 01:03:51 -0700

    form schema refactored

--- models/Forms_Model/Forms_Model.ts
+++ models/Forms_Model/Forms_Model.ts
@@ -1,34 +1,49 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const formsSchema = new Schema(
-  {
-    data: [
-      {
-        question: {
-          type: String,
-          required: [true, "متن سوال را وارد نمایید."],
-        },
-        options: [
-          {
-            value: {
-              type: Schema.Types.Mixed,
-              validate: {
-                validator: function (value: string | number | boolean) {
-                  return (
-                    typeof value === "boolean" ||
-                    typeof value === "string" ||
-                    typeof value === "number"
-                  );
-                },
-                message: "گزینه ها را به درستی وارد نمایید!",
-              },
-            },
-            isTrue: Boolean,
-          },
-        ],
+// Interface for schema
+interface Option {
+  value: string | number | boolean;
+  isTrue?: boolean;
+}
+interface Question {
+  question: string;
+  options: Option[];
+}
+interface Form {
+  data: Question[];
+  user: mongoose.Types.ObjectId;
+}
+interface FormDocument extends Document, Form {}
+
+const optionSchema = new Schema<Option>({
+  value: {
+    type: Schema.Types.Mixed,
+    validate: {
+      validator: function (value: any) {
+        return (
+          typeof value === "boolean" ||
+          typeof value === "string" ||
+          typeof value === "number"
+        );
       },
-    ],
+      message: "گزینه ها را به درستی وارد نمایید!",
+    },
+  },
+  isTrue: Boolean,
+});
+
+const questionSchema = new Schema<Question>({
+  question: {
+    type: String,
+    required: [true, "متن سوال را وارد نمایید."],
+  },
+  options: [optionSchema],
+});
+
+const formsSchema = new Schema<FormDocument>(
+  {
+    data: [questionSchema],
     user: {
       type: Schema.Types.ObjectId,
       ref: "Users",
@@ -39,6 +54,15 @@ const formsSchema = new Schema(
   },
 );
 
-const formsModel = mongoose.model(`Forms`, formsSchema);
+formsSchema.pre(/^find/, function (next) {
+  // @ts-ignore
+  this.populate({
+    path: "user",
+  });
+
+  next();
+});
+
+const formsModel = mongoose.model<FormDocument>(`Forms`, formsSchema);
 
 export default formsModel;
